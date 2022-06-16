993f030b29a03927a3b31f800b740c191c44f0f7
kwons <kwons@DESKTOP-FV81F8F>
2022-06-16 20:51:24 +0900

    메뉴다오수정 메뉴 서비스수정 스토어디테일수정 스토어디테일서블릿추가 메뉴sql수정 패키지 menu.model.dao->menu.dao 로 수정

---
+++ GDJ47_kkoruruk_semi/src/main/java/com/siksin/store/controller/StoreDetailServlet.java
@@ -0,0 +1,95 @@
+package com.siksin.store.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.siksin.menu.model.service.MenuService;
+import com.siksin.menu.model.vo.Menu;
+
+/**
+ * Servlet implementation class StoreDetailServlet
+ */
+@WebServlet("/storedetail.do")
+public class StoreDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StoreDetailServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String storeId="A";
+		
+		
+		
+		int cPage;
+		int numPerpage=5;
+		try {
+			cPage=Integer.parseInt(request.getParameter("cPage"));
+		}catch(NumberFormatException e) {
+			cPage=1;
+		}
+		
+		List<Menu> menuList=new MenuService().searchStoreDeatil(storeId, cPage, numPerpage);
+		
+		int totalData=0;
+		int totalPage=(int)Math.ceil((double)totalData/numPerpage);
+		int pageBarSize=5;
+		int pageNo=((cPage-1)/pageBarSize)*pageBarSize+1;
+		int pageEnd=pageNo+pageBarSize-1;
+		
+		String pageBar="";
+		if(pageNo==1) {
+			pageBar+="<span>[이전]</span>";
+		}else {
+			pageBar+="<a href='"+request.getRequestURL()
+				+"?cPage="+(pageNo-1)+"&searchMenu="+storeId+"'>[이전]</a>";
+		}
+		
+		while(!(pageNo>pageEnd||pageNo>totalPage)) {
+			if(pageNo==cPage) {
+				pageBar+="<span>"+pageNo+"</span>";
+			}else {
+				pageBar+="<a href='"+request.getRequestURL()
+						+"?cPage="+(pageNo)+"&searchMenu="+storeId+"'>"+pageNo+"</a>";
+			}
+			pageNo++;
+		}
+		
+		if(pageNo>totalPage) {
+			pageBar+="<span>[다음]</span>";
+		}else {
+			pageBar+="<a href='"+request.getRequestURL()
+			+"?cPage="+(pageNo)+"&searchMenu="+storeId+"'>[다음]</a>";
+		}
+		request.setAttribute("pageBar", pageBar);
+		request.setAttribute("menuList", menuList);
+		
+		
+				
+		request.getRequestDispatcher("/views/store/storeDetail.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
